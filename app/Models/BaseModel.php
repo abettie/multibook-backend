4e0a4e55f1c0b54911da010ef790e7fae0590e46
Toshiyuki Abe <abe@makedara.biz>
2024-12-01 00:24:18 +0900

    デフォルトでcreated_at, updated_atは取得しないように変更。

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -13,4 +13,10 @@ class BaseModel extends Model
             'updated_at' => 'datetime:Y-m-d H:i:s',
         ];
     } 
+
+    // デフォルトでcreated_at, updated_atは取得しない。
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
 }
