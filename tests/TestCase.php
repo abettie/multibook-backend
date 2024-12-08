6be1c8b9d43c8364df6f6e9fb7806826cff9408f
Toshiyuki Abe <abe@makedara.biz>
2024-12-08 14:03:40 +0900

    PHPUnit時のDB初期化事故防止。

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,9 +8,24 @@ abstract class TestCase extends BaseTestCase
 {
     protected static bool $migrated = false;
 
+    public static function setUpBeforeClass(): void
+    {
+        if (!is_file('.env.testing')) {
+            echo "Pleace create .env.testing for connecting the test DB.\n";
+            echo "Test aborted.\n";
+            exit;
+        }
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
+
+        if (strpos(env('DB_DATABASE'), 'test') === false) {
+            echo "DB name must include 'test'.\n";
+            echo "Test aborted.\n";
+            exit;
+        }
 
         if (!self::$migrated) {
             $this->artisan('config:clear');
@@ -21,4 +36,3 @@ abstract class TestCase extends BaseTestCase
         }
     }
 }
-        
