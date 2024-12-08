4d97cfa2cf4734aae0cd2cac26d0386e64827eeb
Toshiyuki Abe <abe@makedara.biz>
2024-12-08 10:20:25 +0900

    テスト用DBで単体テストをするように変更。

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,19 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected static bool $migrated = false;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (!self::$migrated) {
+            $this->artisan('config:clear');
+            $this->artisan('cache:clear');
+            $this->artisan('migrate:fresh');
+            $this->artisan('db:seed');
+            self::$migrated = true;
+        }
+    }
 }
+        
