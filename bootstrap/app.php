e79724d17604dd3bdcb758af4da64e97ef54cccd
Toshiyuki Abe <abe@makedara.biz>
2025-02-06 12:52:41 +0900

    DataExceptionのログ出力を縮小。

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Exceptions\DataException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -19,4 +20,5 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(fn (DataException $e) => response()->json(['message' => $e->getMessage()], 400));
+        $exceptions->report(fn (DataException $e) => Log::error($e->getMessage()))->stop();
     })->create();
